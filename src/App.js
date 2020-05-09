6e227c24af01e8c7e88e6a80b181a6a150f66215
Daksh Khetan <2012daksh@gmail.com>
2020-05-09 19:31:27 +0530

    add shop page with collection-items component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
