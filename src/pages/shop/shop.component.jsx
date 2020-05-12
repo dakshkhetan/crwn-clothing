966dae5ff47270d369cf980c72d0b63018b8c69a
Daksh Khetan <2012daksh@gmail.com>
2020-05-12 23:15:30 +0530

    install reselect library & fix typo

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
   constructor(props) {
