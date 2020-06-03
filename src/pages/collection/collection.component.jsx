b97184654c4def8b8ab26b2f94df93e5b260a221
Daksh Khetan <2012daksh@gmail.com>
2020-06-03 16:53:10 +0530

    add collection routing & selector

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  return (
+    <div className='collection-page'>
+      <h2>Collection Page</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
