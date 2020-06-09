dae0536759451fa2a722313b6bec929d43d7de3f
Daksh Khetan <2012daksh@gmail.com>
2020-06-09 22:58:36 +0530

    move sign-out to sagas

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -13,7 +13,15 @@ const userReducer = (state = INITIAL_STATE, action) => {
         currentUser: action.payload,
         error: null
       };
+    case UserActionTypes.SIGN_OUT_SUCCESS:
+      return {
+        ...state,
+        currentUser: null,
+        error: null
+      };
     case UserActionTypes.SIGN_IN_FAILURE:
+    case UserActionTypes.SIGN_OUT_FAILURE:
+    case UserActionTypes.SIGN_UP_FAILURE:
       return {
         ...state,
         error: action.payload
