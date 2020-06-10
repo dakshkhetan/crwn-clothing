7c0d9eeaa628e85c08db0389ca8ae777240c3123
Daksh Khetan <2012daksh@gmail.com>
2020-06-10 19:06:29 +0530

    setup stripe-checkout backend (payment API)

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_5VurBgbHG0U1YsXcNaN35LmU00SqPwA51B';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then((response) => {
+        alert('Payment Succesful!');
+      })
+      .catch((error) => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
